edc0195f84d2818168460cba016d6fd396a7cf7e
傑克 <viscont57@gmail.com>
2026-02-22 17:16:22 +0800

    Update submit.js

--- netlify/functions/submit.js
+++ netlify/functions/submit.js
@@ -1,41 +1,19 @@
-exports.handler = async (event) => {
-  console.log("Incoming event:", event);
+exports.handler = async function(event, context) {
+  // 不管是 GET 還是 POST，全部通通接受！
+  // 也不讀取環境變數，單純測試連線。
+  
+  console.log("收到請求！方法是：", event.httpMethod); // 這會寫入 Netlify Log
 
-  if (event.rawMethod !== "POST") {
-    return {
-      statusCode: 405,
-      body: "Method Not Allowed",
-    };
-  }
-
-  const makeUrl = process.env.MAKE_WEBHOOK_URL;
-
-  if (!makeUrl) {
-    return {
-      statusCode: 500,
-      body: "Webhook URL not set",
-    };
-  }
-
-  try {
-    const response = await fetch(makeUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: event.body,
-    });
-
-    return {
-      statusCode: 200,
-      body: "Success",
-    };
-
-  } catch (error) {
-    console.error(error);
-    return {
-      statusCode: 500,
-      body: "Server error",
-    };
-  }
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      status: "Success",
+      message: "恭喜！你的網頁成功連到後端了！",
+      method: event.httpMethod, // 告訴我們它是用什麼方法連進來的
+      hasBody: !!event.body
+    })
+  };
 };
